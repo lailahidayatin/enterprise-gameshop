9c668a6d069b8c4ddf1eebc043bb5c27665d51ef
Laila_Hidayatn <lhidayatin@gmail.com>
2022-07-13 21:44:33 +0700

    wip:psitive case success registration with valid data

--- Test Suites/Running Test.ts
+++ Test Suites/Running Test.ts
@@ -5,10 +5,10 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>08b5be6f-1b7c-4f9a-b59f-8bf473018ffb</testSuiteGuid>
 </TestSuiteEntity>
